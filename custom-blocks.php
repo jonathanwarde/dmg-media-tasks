52f0f21f8620f41c67ee6fed57a9737a53f5eb50
Jon Warde <jon.warde@7dots.co.uk>
2023-10-13 09:41:15 +0100

    WP_CLI command for searching posts within a date range and containing 'add-post-button' guttenberg block

--- custom-blocks.php
+++ custom-blocks.php
@@ -19,3 +19,48 @@ function custom_blocks_enqueue() {
     );
 }
 add_action( 'enqueue_block_editor_assets', 'custom_blocks_enqueue' );
+
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
+
+    class DMG_Read_More_Command extends WP_CLI_Command {
+
+        /**
+         *  Search for posts containing the Gutenberg block within a date range.
+         *  wp dmg-read-more search --date-before="2021-12-31" --date-after="2021-01-01"
+         */
+        public function search( $args, $assoc_args ) {
+            
+            $date_before = isset( $assoc_args['date-before'] ) ? $assoc_args['date-before'] : date( 'Y-m-d', strtotime( '-30 days' ) );
+            $date_after = isset( $assoc_args['date-after'] ) ? $assoc_args['date-after'] : date( 'Y-m-d' );
+
+            $args = array(
+                'post_type' => 'post',
+                'posts_per_page' => -1, // TODO: perf consideration searching thousands of records
+                'date_query' => array(
+                    array(
+                        'after' => $date_after,
+                        'before' => $date_before,
+                        'inclusive' => true,
+                    ),
+                ),
+                's' => '<!-- wp:custom-blocks/add-post-button', 
+            );
+
+            $query = new WP_Query( $args );
+
+            if ( $query->have_posts() ) {
+                while ( $query->have_posts() ) {
+                    $query->the_post();
+                    WP_CLI::log( get_the_ID() );
+                }
+                wp_reset_postdata();
+            } else {
+                WP_CLI::log( 'No posts found.' );
+            }
+        }
+    }
+
+    WP_CLI::add_command( 'dmg-read-more', 'DMG_Read_More_Command' );
+
+}
+
